06c9e3b2e0a96dd3cf63bb63c9b85877c2fcb021
Alexandre-HCS <holy.cow3728@gmail.com>
2024-08-05 16:17:08 -0300

    V4
    Testes 1

--- piscar_2_leds/piscar_2_leds.ino
+++ piscar_2_leds/piscar_2_leds.ino
@@ -1,16 +1,20 @@
 //Progama mucho loko que envia um sinal alto à saída 7, aguarda 5 segundos e manda um sinal baixo para a saída 7 daí repete :P
+//Progama mucho loko que envia um sinal baixo à saída 8, aguarda 5 segundos e manda um sinal alto para a saída 8 daí repete :P
 void setup() {
   // put your setup code here, to run once:
   pinMode(7,OUTPUT);
+  pinMode(8,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-//  aqui o 7 tá alto
+//  aqui o 7 tá alto e o 8 tá baixo
   digitalWrite(7,HIGH);
+  digitalWrite(8,LOW);
   delay(5000);
-  //  aqui o 7 tá baixo
+  //  aqui o 8 tá alto e o 7 tá baixo
   digitalWrite(7,LOW);
+  digitalWrite(8,HIGH);
   delay(5000);
   
 }
