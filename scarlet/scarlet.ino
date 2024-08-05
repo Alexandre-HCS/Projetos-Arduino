43abbc34f003be2c493d7bb4dff00ab91d12892f
Alexandre H. C. Silva <holy.cow3728@gmail.com>
2024-08-05 15:25:46 -0300

    V3

--- scarlet/scarlet.ino
+++ scarlet/scarlet.ino
@@ -1,4 +1,4 @@
-//Progama mucho loko que envia um sinal alto à saída 7, aguarda 5 segundos e manda um sinal baixo para a saída 7 daí repete
+//Progama mucho loko que envia um sinal alto à saída 7, aguarda 5 segundos e manda um sinal baixo para a saída 7 daí repete :P
 void setup() {
   // put your setup code here, to run once:
   pinMode(7,OUTPUT);
